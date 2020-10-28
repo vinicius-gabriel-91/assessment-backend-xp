55bc5f4a041f92dbc49b73cc7a41bab3ebe5dc29
Vinicius Santos <vinicius.gsantos.91@gmail.com>
2020-10-28 11:41:30 -0300

    criando tabela de relacionamento

--- scripts/ecommerce.sql
+++ scripts/ecommerce.sql
@@ -1,52 +1,108 @@
--- MySQL Workbench Forward Engineering
+-- MySQL dump 10.13  Distrib 5.7.32, for Linux (x86_64)
+--
+-- Host: localhost    Database: ecommerce
+-- ------------------------------------------------------
+-- Server version	5.7.32
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema ecommerce
--- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `ecommerce` ;
+--
+-- Table structure for table `categoria`
+--
 
--- -----------------------------------------------------
--- Schema ecommerce
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `ecommerce` DEFAULT CHARACTER SET utf8mb4 ;
-USE `ecommerce` ;
+DROP TABLE IF EXISTS `categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categoria` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `codigo` varchar(255) NOT NULL COMMENT 'codigo da categoria',
+  `nome` varchar(255) DEFAULT NULL COMMENT 'nome da categoria',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `codigo_UNIQUE` (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `ecommerce`.`categoria`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecommerce`.`categoria` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'primary key',
-  `created_time` DATETIME NULL DEFAULT NULL COMMENT 'created tiem',
-  `updated_time` DATETIME NULL DEFAULT NULL COMMENT 'updated tiem',
-  `codigo` VARCHAR(255) NULL DEFAULT NULL COMMENT 'codigo da categoria',
-  `nome` VARCHAR(255) NULL DEFAULT NULL COMMENT 'nome da categoria',
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+--
+-- Dumping data for table `categoria`
+--
 
+LOCK TABLES `categoria` WRITE;
+/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `ecommerce`.`produto`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ecommerce`.`produto` (
-  `id` INT(11) NOT NULL,
-  `Nome` VARCHAR(100) NOT NULL,
-  `SKU` VARCHAR(45) NOT NULL COMMENT 'chave primaria',
-  `preço` DECIMAL(10,0) NULL DEFAULT NULL COMMENT 'valor do produto',
-  `descrição` TEXT NULL DEFAULT NULL COMMENT 'descrição do produto',
-  `quantidade` FLOAT NULL DEFAULT NULL COMMENT 'quantidade de itens',
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+--
+-- Table structure for table `produto`
+--
 
+DROP TABLE IF EXISTS `produto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `produto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nome` varchar(100) NOT NULL,
+  `SKU` varchar(45) NOT NULL COMMENT 'chave primaria',
+  `preço` decimal(10,0) NOT NULL DEFAULT '0' COMMENT 'valor do produto',
+  `descrição` text NOT NULL COMMENT 'descrição do produto',
+  `quantidade` float NOT NULL DEFAULT '0' COMMENT 'quantidade de itens',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `SKU_UNIQUE` (`SKU`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+--
+-- Dumping data for table `produto`
+--
+
+LOCK TABLES `produto` WRITE;
+/*!40000 ALTER TABLE `produto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `produto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `produto_categoria`
+--
+
+DROP TABLE IF EXISTS `produto_categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `produto_categoria` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `produto_id` int(11) NOT NULL,
+  `categoria_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_produto_categoria_categoria1_idx` (`categoria_id`),
+  KEY `fk_produto_categoria_produto1_idx` (`produto_id`),
+  CONSTRAINT `fk_produto_categoria_categoria1` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_produto_categoria_produto1` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produto_categoria`
+--
+
+LOCK TABLES `produto_categoria` WRITE;
+/*!40000 ALTER TABLE `produto_categoria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `produto_categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-10-28 11:39:55
