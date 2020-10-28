a40c7f38c55dc716df5164f2997644eed5f32342
Vinicius Santos <vinicius.gsantos.91@gmail.com>
2020-10-28 10:48:39 -0300

    Criando tabela produtos

--- scripts/ecommerce.sql
+++ scripts/ecommerce.sql
@@ -1,53 +1,52 @@
--- MySQL dump 10.13  Distrib 5.7.32, for Linux (x86_64)
---
--- Host: localhost    Database: ecommerce
--- ------------------------------------------------------
--- Server version	5.7.32
+-- MySQL Workbench Forward Engineering
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
---
--- Table structure for table `categoria`
---
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema ecommerce
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `ecommerce` ;
 
-DROP TABLE IF EXISTS `categoria`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `categoria` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'primary key',
-  `created_time` datetime DEFAULT NULL COMMENT 'created tiem',
-  `updated_time` datetime DEFAULT NULL COMMENT 'updated tiem',
-  `codigo` varchar(255) DEFAULT NULL COMMENT 'codigo da categoria',
-  `nome` varchar(255) DEFAULT NULL COMMENT 'nome da categoria',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- -----------------------------------------------------
+-- Schema ecommerce
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `ecommerce` DEFAULT CHARACTER SET utf8mb4 ;
+USE `ecommerce` ;
 
---
--- Dumping data for table `categoria`
---
+-- -----------------------------------------------------
+-- Table `ecommerce`.`categoria`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ecommerce`.`categoria` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `created_time` DATETIME NULL DEFAULT NULL COMMENT 'created tiem',
+  `updated_time` DATETIME NULL DEFAULT NULL COMMENT 'updated tiem',
+  `codigo` VARCHAR(255) NULL DEFAULT NULL COMMENT 'codigo da categoria',
+  `nome` VARCHAR(255) NULL DEFAULT NULL COMMENT 'nome da categoria',
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-LOCK TABLES `categoria` WRITE;
-/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
-/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- -----------------------------------------------------
+-- Table `ecommerce`.`produto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ecommerce`.`produto` (
+  `id` INT(11) NOT NULL,
+  `Nome` VARCHAR(100) NOT NULL,
+  `SKU` VARCHAR(45) NOT NULL COMMENT 'chave primaria',
+  `preço` DECIMAL(10,0) NULL DEFAULT NULL COMMENT 'valor do produto',
+  `descrição` TEXT NULL DEFAULT NULL COMMENT 'descrição do produto',
+  `quantidade` FLOAT NULL DEFAULT NULL COMMENT 'quantidade de itens',
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
 
--- Dump completed on 2020-10-28  7:31:13
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
